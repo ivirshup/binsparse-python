ea30efa61c4fad816b7243dea0b34dca4782f98d
taddyb <tkb5476@psu.edu>
2024-03-12 07:36:26 -0400

    added better testing for equality in graphblas

--- src/binsparse/_testing.py
+++ src/binsparse/_testing.py
@@ -1,10 +1,11 @@
 from functools import singledispatch
 
+import graphblas as gb
 import numpy as np
 from scipy import sparse
 
 
-def assert_equal(a: sparse.spmatrix, b):
+def assert_equal(a: gb.Matrix | sparse.spmatrix, b):
     assert type(a) == type(b), f"types differ: {type(a)} != {type(b)}"
     assert a.shape == b.shape, f"shapes differ: {a.shape} != {b.shape}"
     _assert_equal(a, b)
@@ -26,3 +27,9 @@ def _(a, b):
 def _(a, b):
     for attr in ["row", "col", "data"]:
         np.testing.assert_equal(getattr(a, attr), getattr(b, attr))
+
+
+@_assert_equal.register(gb.Matrix)
+def _(a, b):
+    equal = a.isequal(b)
+    assert equal, "GraphBLAS matrices are not equal"
